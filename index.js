8571f0e45e9fcd9660f6fb4278d46bfd7cfb977f
Marisa Triola <marisatriola@gmail.com>
2022-03-13 12:32:03 -0400

    fix environment variable issue
    remove env
    
    remove env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, Intents, Message } = require('discord.js');
 const axios = require('axios');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+require('dotenv').config();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -17,5 +18,5 @@ getUrl = async () => {
     const res = await axios.get('https://inspirobot.me/api?generate=true');
     return await res.data;
 }
- 
+console.log(process.env.TOKEN); 
 client.login(process.env.TOKEN);
