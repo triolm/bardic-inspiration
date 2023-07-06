a1ef2564400d21bbe90f939bfc79fee55e170ea6
Marisa Triola <marisatriola@gmail.com>
2023-07-06 00:01:37 -0400

    uninspire

--- index.js
+++ index.js
@@ -12,11 +12,22 @@ client.on('message', async message => {
         const url = await getUrl();
         await message.channel.send(url);
     }
+    else if (message.content.trim().toLowerCase() == "!don't inspire") {
+        const url = await getUninspiringUrl();
+        await message.channel.send(url);
+    }
 });
 
 getUrl = async () => {
     const res = await axios.get('https://inspirobot.me/api?generate=true');
     return await res.data;
 }
+
+getUninspiringUrl = async () => {
+    const res = await axios.get('https://source.unsplash.com/random/600x600');
+    return await res.data;
+}
+
+
 console.log(process.env.TOKEN); 
 client.login(process.env.TOKEN);
